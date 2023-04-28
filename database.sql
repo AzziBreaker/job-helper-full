12b1fa0148a3d327bf3d60fcfec9dec5324544b4
Denis Kiprov <deniskiprov93@gmail.com>
2023-04-28 12:31:45 +0300

    ADEED A LOT OF STUFF YEAHHHH

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS jobHelper;
 CREATE DATABASE jobHelper;
 USE jobHelper;
 CREATE TABLE users(
@@ -5,6 +6,6 @@ id INT,
 username VARCHAR(255),
 email VARCHAR(255),
 phone VARCHAR(255),
-password VARCHAR(255)
+pass VARCHAR(255)
 );
 INSERT INTO users VALUES (1, "admin", "admin@local.bg", "0888948064", "admin");
--- src/java/RegisterServlet.java
+++ src/java/RegisterServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,9 +30,22 @@ public class RegisterServlet extends HttpServlet {
                    </html>
                    """;
         String db = "jdbc:mariadb://localhost/jobHelper";
+        String username = request.getParameter("username");
+        String email= request.getParameter("email");;
+        String phone= request.getParameter("phone");;
+        String pass= request.getParameter("password");; 
+        
         try {
-            Class.forName("org.mariadb.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("db");
+            Class.forName("org.mariadb.jdbc.Driver");   
+            Connection conn = DriverManager.getConnection(db);
+            Statement stmt = conn.createStatement();
+            String query = """ 
+                           INSERT INTO users(username, email, phone, pass) 
+                           VALUES (\"%s\", \"%s\", \"%s\", \"%s\");
+                           """.formatted(username, email, phone, pass);
+            ResultSet result = stmt.executeQuery(query);
+            
+            
         } catch (SQLException ex) {
             Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
